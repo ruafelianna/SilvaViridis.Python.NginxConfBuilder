35c053e4d34e25fac89daefd29099f235400c335
Maria Sorokina <ruafelianna@ya.ru>
2025-03-25 14:58:20 +0300

    Add Docker default Nginx conf

--- src/SilvaViridis/Exe/NginxConfBuilder/Defaults/__init__.py
+++ src/SilvaViridis/Exe/NginxConfBuilder/Defaults/__init__.py
@@ -1,5 +1,7 @@
 from ._debian_12 import get_debian_12_default_conf
+from ._docker import get_docker_default_conf
 
 __all__ = [
     "get_debian_12_default_conf",
+    "get_docker_default_conf",
 ]
---
+++ src/SilvaViridis/Exe/NginxConfBuilder/Defaults/_docker.py
@@ -0,0 +1,110 @@
+from SilvaViridis.Python.Common.Web import MimeType
+
+from ..Common import (
+    LogLevel,
+    OnOff,
+    Path,
+    TimeInterval,
+)
+
+from ..Modules import MainContext
+
+from ..Modules.Core import (
+    ErrorLog,
+    Events,
+    Include,
+    Pid,
+    User,
+    WorkerConnections,
+    WorkerProcesses
+)
+
+from ..Modules.HttpCore import (
+    DefaultType,
+    Http,
+    KeepaliveTimeout,
+    Sendfile,
+)
+
+from ..Modules.HttpCore.Variables import (
+    VAR_REMOTE_ADDR,
+    VAR_REMOTE_USER,
+    VAR_TIME_LOCAL,
+    VAR_REQUEST,
+    VAR_STATUS,
+    VAR_BODY_BYTES_SENT,
+    HttpVariable,
+)
+
+from ..Modules.HttpLog import (
+    AccessLog,
+    LogFormat,
+)
+
+def get_docker_default_conf(
+) -> MainContext:
+    return MainContext(
+        user = User(
+            order = 100,
+            user = "nginx",
+        ),
+        worker_processes = WorkerProcesses(
+            order = 200,
+            quantity = "auto",
+        ),
+        error_log = ErrorLog(
+            order = 300,
+            file = Path("/var/log/nginx/error.log"),
+            level = LogLevel.notice,
+        ),
+        pid = Pid(
+            order = 400,
+            file = Path("/var/run/nginx.pid"),
+        ),
+        events = Events(
+            order = 500,
+            worker_connections = WorkerConnections(
+                order = 100,
+                quantity = 1024,
+            ),
+        ),
+        http = Http(
+            order = 600,
+            include_list = [
+                Include(
+                    order = 100,
+                    file = Path("/etc/nginx/mime.types"),
+                ),
+                Include(
+                    order = 700,
+                    file = Path("/etc/nginx/conf.d/*.conf"),
+                ),
+            ],
+            default_type = DefaultType(
+                order = 200,
+                mime_type = MimeType.application__octet_stream,
+            ),
+            log_format = LogFormat(
+                order = 300,
+                name = "main",
+                strings = [
+                    f"{VAR_REMOTE_ADDR} - {VAR_REMOTE_USER} [{VAR_TIME_LOCAL}] "
+                    + f"\"{VAR_REQUEST}\" {VAR_STATUS} {VAR_BODY_BYTES_SENT} "
+                    + f"\"{HttpVariable("referer")}\" \"{HttpVariable("user_agent")}\" \"{HttpVariable("x_forwarded_for")}\"",
+                ],
+            ),
+            access_log = AccessLog(
+                order = 400,
+                path = Path("/var/log/nginx/access.log"),
+                format = "main",
+            ),
+            sendfile = Sendfile(
+                order = 500,
+                state = OnOff.on,
+            ),
+            keepalive_timeout = KeepaliveTimeout(
+                order = 600,
+                time = TimeInterval(65),
+            ),
+        ),
+    )
